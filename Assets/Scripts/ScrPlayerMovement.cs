3158ec17819b50b56c1e9a9a9123a6d32399c85d
__init__ <aavendanoca@unal.edu.co>
2025-01-23 21:45:53 -0500

    Fix player rotation

--- Assets/Scripts/ScrPlayerMovement.cs
+++ Assets/Scripts/ScrPlayerMovement.cs
@@ -77,7 +77,7 @@ public class ScrPlayerMovement : MonoBehaviour
             _t = 0;
             _initialPosition = gameObject.transform.position;
             _finalPosition = _initialPosition + _dir;
-            _childMesh.transform.rotation = Quaternion.LookRotation(-_dir, transform.up);
+            transform.rotation = Quaternion.LookRotation(-_dir, transform.up);
         }
 
         _t += Time.deltaTime * moveSpeed;
